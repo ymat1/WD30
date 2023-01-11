e5b1d7835e8a4f1d7e9bf7a95c043165cb775557
Raymart Barrameda <matbarrameda@gmail.com>
2023-01-11 18:39:11 +0800

    01112023a

--- n01102023/activity/scripts.js
+++ n01102023/activity/scripts.js
@@ -1,38 +1,90 @@
-const operand = document.querySelector('.operand');
-const result = document.querySelector('.result');
-const buttons = document.querySelectorAll('button');
+const calculator = document.querySelector('.calculator');
+const display = document.querySelector('.display');
 
-let calculation = [];
-let inputs;
+let firstOperand = null;
+let operator = null;
+let waitingForSecondOperand = false;
 
-function calculate(button) {
-    const value = button.innerText;
-    if(value === 'CLEAR') {
-        calculation = [];
-        operand.innerText = '';
-        result.innerText = '0';
-    }
-    else if(value === 'DEL') {
-        calculation.pop(value);
-        inputs = calculation.join('');
-        operand.innerText = inputs;
-    }
-    else if(value === "=") {
-        result.innerText = eval(inputs);
-    }
-    else {
-        calculation.push(value);
-        inputs = calculation.join('');
-        operand.innerText = inputs;
-    }
-}
-
-buttons.forEach(
-    button => button.addEventListener(
-        'click',
-        () => {
-            result.innerText = '';
-            calculate(button);
+calculator.addEventListener(
+    'click',
+    e => {
+        performComputation = (firstOperand, operator, secondOperand) => {
+            if(operator === '+') {
+                return firstOperand + secondOperand;
+            }
+            else if(operator === '-') {
+                return firstOperand - secondOperand;
+            }
+            else if(operator === '*') {
+                return firstOperand * secondOperand;
+            }
+            else if(operator === '/') {
+                return firstOperand / secondOperand;
+            }
         }
-    )
+        if(e.target.classList.contains('number')) {
+            inputNumber = (number) => {
+                const displayValue = display.innerText;
+                if(waitingForSecondOperand) {
+                    display.innerText = number;
+                    waitingForSecondOperand = false;
+                }
+                else {
+                    display.innerText = displayValue === '0' ? number : displayValue + number;
+                }
+            }
+            inputNumber(e.target.innerText);
+        }
+        else if(e.target.classList.contains('operator')) {
+            inputOperator = (op) => {
+                const displayValue = display.innerText;
+                if(firstOperand === null) {
+                    firstOperand = parseFloat(displayValue);
+                }
+                else if(operator) {
+                    const result = performComputation(firstOperand, operator, parseFloat(displayValue));
+                    display.innerText = result;
+                    firstOperand = result;
+                }
+                operator = op;
+                waitingForSecondOperand = true;
+            }
+            inputOperator(e.target.innerText);
+        }
+        else if(e.target.classList.contains('clear')) {
+            clear = () => {
+                firstOperand = null;
+                operator = null;
+                waitingForSecondOperand = false;
+                display.innerText = '0';
+            }
+            clear();
+        }
+        else if(e.target.classList.contains('delete')) {
+            // del = () => {
+        
+            // }
+            del();
+        }
+        else if(e.target.classList.contains('decimal')) {
+            inputDecimal = () => {
+                const displayValue = display.innerText;
+                if(!displayValue.includes('.')) {
+                    display.innerText = displayValue + '.';
+                }
+            }
+            inputDecimal();
+        }
+        else if(e.target.classList.contains('equal')) {
+            compute = () => {
+                if(firstOperand && operator && !waitingForSecondOperand) {
+                    const result = performComputation(firstOperand, operator, parseFloat(display.innerText));
+                    display.innerText = result;
+                    firstOperand = null;
+                    operator = null;
+                }
+            }
+            compute();
+        }
+    }
 );
